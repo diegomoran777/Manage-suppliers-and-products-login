98c9e677d888e18674b79827577ca499c6f6811c
Diego Moran <32073494+diegomoran777@users.noreply.github.com>
2020-06-09 15:05:25 -0300

    Add files via upload

---
+++ Angular/components/nav/nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  activateRole:string;
+  userName:string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.activateRole = sessionStorage.getItem('role');
+    this.userName = sessionStorage.getItem('userName');
+  }
+
+}
