b769ff3d1a70a46879ba900a4bf91fa07d3fee56
Diego Moran <32073494+diegomoran777@users.noreply.github.com>
2020-06-09 15:21:57 -0300

    Add files via upload

---
+++ nodejs/dto/types.js
@@ -0,0 +1,15 @@
+
+class Types {
+
+    types = new Set();
+
+    getTypes() {
+         return this.types;
+    };
+
+    clearTypes() {
+        this.types.clear();
+    };
+}
+
+module.exports = Types;
