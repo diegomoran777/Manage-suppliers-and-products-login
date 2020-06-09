f2e37a79dd423a2915eaab2a9367d53d3eec9c3f
Diego Moran <32073494+diegomoran777@users.noreply.github.com>
2020-06-09 16:18:31 -0300

    Add files via upload

---
+++ nodejs/app.js
@@ -0,0 +1,46 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+
+let supplierRoute = require('./routes/supplierRoute');
+let productRoute = require('./routes/productRoute');
+let userRoute = require('./routes/userRoute');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.all("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
+
+app.use('/api/supplier', supplierRoute);
+app.use('/api/product', productRoute);
+app.use('/api/user', userRoute);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
